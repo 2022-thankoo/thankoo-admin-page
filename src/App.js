40f373f5a2e774b8bb7a13b9afccd1bf2cf50b66
skullKim <kyk990328@gmail.com>
2022-09-03 19:55:35 +0900

    feat: 전역 css 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import AsidePage from "./component/aside/Aside";
+import GlobalStyle from "./component/style/GlobalStyle";
+import category from "./data/AsideCategory";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      <GlobalStyle />
+      <AsidePage category={category}/>
+    </>
   );
 }
 
