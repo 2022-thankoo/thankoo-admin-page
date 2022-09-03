f859873987d4985252d11bf456dbcf5df16991d7
skullKim <kyk990328@gmail.com>
2022-09-03 22:50:34 +0900

    feat: 전체 선택 기능 완료

---
+++ src/component/dataList/DataList.js
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+import {RiDeleteBin5Fill} from "react-icons/ri";
+import {GiAutoRepair} from "react-icons/gi";
+import {useState} from "react";
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+const TableHeader = styled.thead`
+  height: 40px;
+  background-color: #C1C1C1;
+`;
+
+const Row = styled.tr`
+  text-align: center;
+  border-bottom: 1px solid black;
+`;
+
+const TableBody = styled.tbody`
+  height: 100%;
+  overflow: scroll;
+`;
+
+function DataList() {
+
+  const [allSelected, setAllSelected] = useState(false);
+  const [selectedList, setSelectedList] = useState([]);
+  const idList = [1, 2];
+
+  const handleSelectAll = ({target: {checked}}) => {
+    setSelectedList(checked ? idList : []);
+  }
+
+  const handleSelect = ({target: {checked}}, id) => {
+    if (checked) {
+      setSelectedList([...selectedList, id]);
+      return;
+    }
+    setSelectedList(selectedList.filter(selectedId => selectedId !== id));
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <tr>
+          <th><input type="checkbox" onClick={handleSelectAll} /></th>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>action</th>
+        </tr>
+      </TableHeader>
+      <TableBody>
+       <Row>
+         <td>
+           <input
+             type="checkbox"
+             onClick={e => handleSelect(e, 1)}
+             checked={selectedList.includes(1)}
+           />
+         </td>
+         <td>1</td>
+         <td>skull</td>
+         <td>example@gmail.com</td>
+         <td>
+           <button>
+             <RiDeleteBin5Fill />
+           </button>
+           <button>
+             <GiAutoRepair />
+           </button>
+         </td>
+       </Row>
+
+        <Row>
+          <td>
+            <input
+              type="checkbox"
+              onClick={e => handleSelect(e, 2)}
+              checked={selectedList.includes(2)}
+            />
+          </td>
+          <td>1</td>
+          <td>skull</td>
+          <td>example@gmail.com</td>
+          <td>
+            <button>
+              <RiDeleteBin5Fill />
+            </button>
+            <button>
+              <GiAutoRepair />
+            </button>
+          </td>
+        </Row>
+      </TableBody>
+    </Table>
+  );
+}
+
+export default DataList;
--- src/page/Coupon.js
+++ src/page/Coupon.js
@@ -1,8 +1,15 @@
 import Header from "../component/header/Header";
 import SearchOption from "../data/SearchOption";
+import DataList from "../component/dataList/DataList";
+import PageWrapper from "../component/commonStyle/PageWrapper";
 
 function CouponPage() {
-  return <Header searchOption={SearchOption.coupon}/>
+  return (
+    <PageWrapper>
+      <Header searchOption={SearchOption.coupon}/>
+      <DataList />
+    </PageWrapper>
+  )
 }
 
 export default CouponPage;
