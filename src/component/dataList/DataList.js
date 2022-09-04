b356c09db5531e0bfd1d58f1e780ea07832ab530
skullKim <kyk990328@gmail.com>
2022-09-04 11:58:44 +0900

    feat: 선택 항목 삭제 버튼 구현 완료

--- src/component/dataList/DataList.js
+++ src/component/dataList/DataList.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {RiDeleteBin5Fill} from "react-icons/ri";
 import {GiAutoRepair} from "react-icons/gi";
+import {IoIosArrowDown, IoIosArrowUp} from "react-icons/io";
 import {useState} from "react";
 
 const Table = styled.table`
@@ -23,9 +24,18 @@ const TableBody = styled.tbody`
   overflow: scroll;
 `;
 
+const DropDownWrapper = styled.div`
+  width: 120px;
+  height: auto;
+  position: absolute;
+  right: 50px;
+  background-color: #C1C1C1;
+`;
+
 function DataList() {
 
   const [selectedList, setSelectedList] = useState([]);
+  const [showDropDown, setShowDropDown] = useState(false);
   const idList = [1, 2];
 
   const handleSelectAll = ({target: {checked}}) => {
@@ -40,44 +50,66 @@ function DataList() {
     setSelectedList(selectedList.filter(selectedId => selectedId !== id));
   }
 
+  const handleShowDropDown = () => {
+    setShowDropDown(!showDropDown);
+  }
+
+  const handleDeleteSelected = () => {
+    console.log(selectedList);
+  }
+
   return (
     <Table>
       <TableHeader>
         <tr>
-          <th><input type="checkbox" onClick={e => handleSelectAll(e)} /></th>
+          <th><input type="checkbox" onChange={e => handleSelectAll(e)}/></th>
           <th>id</th>
           <th>name</th>
           <th>email</th>
-          <th>action</th>
+          <th>
+            action
+            {!showDropDown
+              ? <IoIosArrowUp onClick={handleShowDropDown}/>
+              : <>
+                <IoIosArrowDown onClick={handleShowDropDown}/>
+                <DropDownWrapper>
+                  <button
+                    type='button'
+                    onClick={handleDeleteSelected}
+                  >delete selected</button>
+                </DropDownWrapper>
+              </>
+            }
+          </th>
         </tr>
       </TableHeader>
       <TableBody>
-       <Row>
-         <td>
-           <input
-             type="checkbox"
-             onClick={e => handleSelect(e, 1)}
-             checked={selectedList.includes(1)}
-           />
-         </td>
-         <td>1</td>
-         <td>skull</td>
-         <td>example@gmail.com</td>
-         <td>
-           <button>
-             <RiDeleteBin5Fill />
-           </button>
-           <button>
-             <GiAutoRepair />
-           </button>
-         </td>
-       </Row>
+        <Row>
+          <td>
+            <input
+              type="checkbox"
+              onChange={e => handleSelect(e, 1)}
+              checked={selectedList.includes(1)}
+            />
+          </td>
+          <td>1</td>
+          <td>skull</td>
+          <td>example@gmail.com</td>
+          <td>
+            <button>
+              <RiDeleteBin5Fill/>
+            </button>
+            <button>
+              <GiAutoRepair/>
+            </button>
+          </td>
+        </Row>
 
         <Row>
           <td>
             <input
               type="checkbox"
-              onClick={e => handleSelect(e, 2)}
+              onChange={e => handleSelect(e, 2)}
               checked={selectedList.includes(2)}
             />
           </td>
@@ -86,10 +118,10 @@ function DataList() {
           <td>example@gmail.com</td>
           <td>
             <button>
-              <RiDeleteBin5Fill />
+              <RiDeleteBin5Fill/>
             </button>
             <button>
-              <GiAutoRepair />
+              <GiAutoRepair/>
             </button>
           </td>
         </Row>
