b5e3d7f2562105855526e9c6ef4bf556776877f6
skullKim <kyk990328@gmail.com>
2022-10-13 15:21:53 +0900

    feat: 조직 생성 API 연동 완료

--- src/data/message.js
+++ src/data/message.js
@@ -1,6 +1,7 @@
 export const warningMessage = {
   invalidDateFormat: '날짜 포맷이 올바르지 않습니다',
   invalidSearchOptionStatus: '상태가 올바르지 않습니다',
+
   invalidCoach: '존재하지 않는 코치입니다',
   invalidCouponType: '존재하지 않는 쿠폰 타입입니다',
   couponNumberIsNotInteger: '쿠폰 갯수는 정수여야 합니다',
@@ -9,4 +10,9 @@ export const warningMessage = {
   invalidCouponTitleLength: '쿠폰 제목은 20자 이하여야 합니다',
   invalidCouponMessageLength: '쿠폰 메시지는 100자 이하여야 합니다',
   cannotCreateCoupon: '회원 가입을 하지 않아 쿠폰을 생성할 수 없습니다',
+
+  organizationNameIsEmpty: '조직 이름을 입력해 주세요',
+  organizationNameLengthExceeded: '조직 이름은 최대 12글자입니다',
+  organizationLimitedSizeIsEmpty: '조직 인원 수를 입력해 주세요',
+  organizationLimitedSizeError: '조직 인원 수는 10명 이상 500명 이하여야 합니다',
 };
--- src/page/OrganizationCreation.js
+++ src/page/OrganizationCreation.js
@@ -1,33 +1,78 @@
+import * as yup from 'yup';
+import {useFormik} from "formik";
+
 import AuthorizationWrapper from "../component/AuthorizationWrapper";
 import {CentralizeComponentWrapper} from "../component/commonStyle/PageWrapper";
 import {DataInput, DataInputWrapper, InputBox, SubmitButton} from "../component/commonStyle/DataInput";
+import {warningMessage} from "../data/message";
+import {authenticatedRequest} from "../util/axiosIntance";
 
 function OrganizationCreation() {
 
   const requiredOptions = {
-    organizationName: 'organizationName',
+    name: 'name',
     limitedSize: 'limitedSize',
   };
 
   const labelElements = {
-    [requiredOptions.organizationName]: '조직 이름을 입력해주세요',
+    [requiredOptions.name]: '조직 이름을 입력해주세요',
     [requiredOptions.limitedSize]: '조직 인원 제한을 입력해주세요',
   };
+
+  const formik = useFormik({
+    initialValues: {
+      [requiredOptions.name]: '',
+      [requiredOptions.limitedSize]: 0,
+    },
+    validationSchema: yup.object({
+      [requiredOptions.name]: yup.string()
+        .required(warningMessage.organizationNameIsEmpty)
+        .max(12, warningMessage.organizationNameLengthExceeded),
+      [requiredOptions.limitedSize]: yup.number()
+        .required(warningMessage.organizationLimitedSizeIsEmpty)
+        .min(10, warningMessage.organizationLimitedSizeError)
+        .max(500, warningMessage.organizationLimitedSizeError)
+    }),
+    onSubmit: (options) => {
+      const requestBody = {
+        [requiredOptions.name]: options[requiredOptions.name],
+        [requiredOptions.limitedSize]: options[requiredOptions.limitedSize],
+      };
+
+      authenticatedRequest({
+        method: 'POST',
+        url: `${process.env.REACT_APP_SERVER_ORIGIN}/admin/organizations`,
+        data: {...requestBody}
+      })
+        .then(() => alert('조직이 생성되었습니다.'))
+        .catch(err => console.log(err));
+    }
+  });
+
+  const handleChange = (event) => {
+    formik.handleChange(event);
+  }
+
+  const handleCreateOrganization = (event) => {
+    event.preventDefault();
+    formik.handleSubmit(event);
+  }
 
   return (
     <AuthorizationWrapper>
       <CentralizeComponentWrapper>
         <DataInputWrapper>
           {Object.keys(requiredOptions).map(key => (
-            <InputBox>
+            <InputBox key={key}>
               <label htmlFor={requiredOptions[key]}>{labelElements[key]}</label>
               <DataInput
                 type='text'
                 name={requiredOptions[key]}
+                onChange={handleChange}
               />
             </InputBox>
           ))}
-          <SubmitButton type='submit'>
+          <SubmitButton type='submit' onClick={handleCreateOrganization}>
             Create Organization
           </SubmitButton>
         </DataInputWrapper>
